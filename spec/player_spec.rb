343a9f2b0e5783214e607f1b5532eace670deff3
KenTohara <>
2022-07-03 15:20:09 -0400

    Add Player specs

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,23 +1,24 @@
 # frozen_string_literal: true
 
 require 'player'
-require 'board'
+# require 'board'
 
 describe Player do
   subject(:player) { Player.new('Rhubarb', 'x') }
   let(:board) { instance_double(Board) }
-  # before do
-  #   player.instance_variable_set(:@name, 'John')
-  #   player.instance_variable_set(:@symbol, '@')
-  # end
+  let(:symbols) { double('symbols') }
 
   describe '#get_position' do
     before do
-      allow(board).to receive(:valid_move?).and_return true
+      valid_input = '3'
+      allow(player).to receive(:prompt_get_position)
+      allow(player).to receive(:gets).and_return(valid_input)
+      allow(board).to receive(:valid_move?).and_return(true)
     end
 
     it 'should return a position based on user input' do
-
+      expect(player.get_position(board, symbols)).to eq(3)
+      player.get_position(board, symbols)
     end
   end
 end
